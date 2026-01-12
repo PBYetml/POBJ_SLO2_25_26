5d10016f5f747c9098ca0bf58c507115abe60e39
Swiss_Maverick <144990041+SwissMaverick@users.noreply.github.com>
2026-01-12 13:22:45 +0100

    C# demo
    C# demo

--- SolutionEtudiants/JJE/C#/CSharp_Demo/Program.cs
+++ SolutionEtudiants/JJE/C#/CSharp_Demo/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CSharp_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
