1b7f46ccea6d262954aa7ee27ae99d8f6c4dcf96
Swiss_Maverick <144990041+SwissMaverick@users.noreply.github.com>
2026-03-16 14:20:18 +0100

    Create Arme.java

---
+++ DemoJava/Demonstration_Eclipse/src/Demonstration_Eclipse/Arme.java
@@ -0,0 +1,30 @@
+package Demonstation_Eclipse;
+
+public class couteau extends arme
+{
+	
+	//-- Attributs --//
+	// Private
+	private String mTypeArme;
+	private String mModele;
+	
+	// Constructeur
+	public couteau()
+	{
+		super();
+		mDegats = 10;
+	}
+	public couteau(String Modele, int degats)
+	{ }
+	
+	@Override
+	public int Frapper()
+	{
+		int degatsInfliges = 0;
+		
+		return degatsInfilges;
+	}
+	
+	
+	
+}
